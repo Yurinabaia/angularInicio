18ca1558d037c44da1646c4ff9924f39021c2567
Yuri.Duarte <yuri.duarte@conservo.com.br>
2021-06-14 12:58:58 -0300

    Model, o shared significar que todas os model criados aqui são visiviel em todo projeto.

---
+++ src/app/shared/tarefa.model.ts
@@ -0,0 +1,12 @@
+export class Tarefas 
+{
+    constructor(
+        public id?: number,
+        public nome?: string, 
+        public concluido?: boolean//O ponto de interrogação é o Elvis, 
+        //sendo que quando você instanciar a classe você pode ou não passar esse campo
+    ) 
+    {
+
+    }
+}
